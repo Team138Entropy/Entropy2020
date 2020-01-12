2bd108c5626f50c94f91b2d9f6650f9a4f4c4866
Will Leverone <barrelwolf38087@gmail.com>
2020-01-12 09:46:52 -0500

    Begin work on timing system (still using dummy subsystems)

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Timer;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 public class Shooter extends Subsystem {
@@ -7,6 +10,8 @@ public class Shooter extends Subsystem {
     // TODO: Integrate with other subsystems for real
     // TEMPORARY STUFF BEGINS HERE
     private static final int ROLLER_PORT = 0;
+    private static final double ROLLER_SPEED = 0.5;
+    private static final double FIRE_DELAY_SECONDS = 0.5;
 
     private static class TurretPosition {
         private double m_horizontal, m_vertical;
@@ -32,10 +37,16 @@ public class Shooter extends Subsystem {
 
     // TEMPORARY STUFF ENDS HERE
 
+    // Aggregation
     private Shooter instance;
     private WPI_TalonSRX m_roller;
     private Turret m_turret;
     private Vision m_vision;
+    private Timer m_timer;
+
+    // State variables
+    private boolean m_ableToFire = false;
+    private boolean m_fire = false;
 
     public Shooter getInstance() {
         if (instance == null)
@@ -46,22 +57,51 @@ public class Shooter extends Subsystem {
     private Shooter() {
         m_roller = new WPI_TalonSRX(ROLLER_PORT);
 
-        // Replace these with real subsystems
-        m_turret = position -> {};
-        m_vision = () -> new TurretPosition(0, 0);
+        // TODO: Replace these with real subsystems
+        m_turret = position -> System.out.println(
+            "Setting dummy turret position to ("
+            + position.getHorizontal()
+            + ", "
+            + position.getVertical()
+            + ")"
+        );
+        m_vision = () -> {
+            System.out.println("Getting dummy vision target");
+            return new TurretPosition(0, 0);
+        };
+
+        m_timer = new Timer();
+    }
+
+    // Call this in the robot loop.
+    // TODO: Consider replacing this home-grown timing system with WPILib's scheduler.
+    public void periodic() {
+
     }
 
     public void fireSingle() {
-
+//        if (!m_ableToFire) {
+//            m_timer.reset();
+//            m_timer.start();
+//
+//        }
     }
 
     public void fireAuto() {}
 
     public void stopAuto() {}
 
-    public void target() {}
+    public void target() {
+        m_turret.set(m_vision.calcTargetPosition());
+    }
 
+    private void start() {
+        m_roller.set(ControlMode.PercentOutput, ROLLER_SPEED);
+    }
 
+    private void stop() {
+        m_roller.set(ControlMode.PercentOutput, 0);
+    }
 
     @Override
     public void ZeroSensors() {}
