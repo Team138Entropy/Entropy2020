eb45110ef0b009b788487f4b70cbeceb7882c0da
maxh65000 <booah8@gmail.com>
2020-01-18 13:46:40 -0500

    Fix problems with host key verification If you switch between 2 RIOs, the host key cached is wrong

--- deployconfig.sh
+++ deployconfig.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-scp src/main/deploy/config.properties admin@10.1.38.2:/home/lvuser/deploy/config.properties
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null src/main/deploy/config.properties admin@10.1.38.2:/home/lvuser/deploy/config.properties
