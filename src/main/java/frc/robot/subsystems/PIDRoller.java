7f065efd47259a7f28ed6e7fb00eafba77759f34
Will Leverone <barrelwolf38087@gmail.com>
2020-01-19 10:28:37 -0500

    Begin work on PIDRoller class

---
+++ src/main/java/frc/robot/subsystems/PIDRoller.java
@@ -0,0 +1,29 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import edu.wpi.first.wpilibj.controller.PIDController;
+
+class PIDRoller extends Subsystem {
+
+    private WPI_TalonSRX talon;
+    private PIDController pidController;
+
+    PIDRoller(int talonPort, double p, double i, double d) {
+        super();
+
+        talon = new WPI_TalonSRX(talonPort);
+        pidController = new PIDController(p, i, d);
+    }
+
+
+
+    @Override
+    public void ZeroSensors() {
+
+    }
+
+    @Override
+    public void CheckSubsystems() {
+
+    }
+}
