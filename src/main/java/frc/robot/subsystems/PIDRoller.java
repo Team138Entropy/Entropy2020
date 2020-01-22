9cc846a3b3a43e79cc1cea83bda9419d4a3af624
Jay Hinspeter <jay.hinspeter@gmail.com>
2020-01-21 19:35:51 -0500

    Format cow

--- src/main/java/frc/robot/subsystems/PIDRoller.java
+++ src/main/java/frc/robot/subsystems/PIDRoller.java
@@ -1,43 +1,43 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 class PIDRoller {
 
-    private static final int pidLoopIndex = 0;
-    private static final int timeoutMs = 10;
+  private static final int pidLoopIndex = 0;
+  private static final int timeoutMs = 10;
 
-    private WPI_TalonSRX talon;
+  private WPI_TalonSRX talon;
 
-    PIDRoller(int talonPort, double p, double i, double d) {
-        super();
+  PIDRoller(int talonPort, double p, double i, double d) {
+    super();
 
-        talon = new WPI_TalonSRX(talonPort);
+    talon = new WPI_TalonSRX(talonPort);
 
-        // All of this was ripped from the 2019 elevator code
-		talon.configNominalOutputForward(0, timeoutMs);
-		talon.configNominalOutputReverse(0, timeoutMs);
-		talon.configPeakOutputForward(1, timeoutMs);
-        talon.configPeakOutputReverse(-1, timeoutMs);
-        
-        talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, pidLoopIndex, timeoutMs);
-        talon.setSensorPhase(false);
+    // All of this was ripped from the 2019 elevator code
+    talon.configNominalOutputForward(0, timeoutMs);
+    talon.configNominalOutputReverse(0, timeoutMs);
+    talon.configPeakOutputForward(1, timeoutMs);
+    talon.configPeakOutputReverse(-1, timeoutMs);
 
-        talon.configAllowableClosedloopError(pidLoopIndex, 0, timeoutMs);
+    talon.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, pidLoopIndex, timeoutMs);
+    talon.setSensorPhase(false);
 
-        talon.config_kP(pidLoopIndex, p);
-        talon.config_kI(pidLoopIndex, i);
-        talon.config_kD(pidLoopIndex, d);
+    talon.configAllowableClosedloopError(pidLoopIndex, 0, timeoutMs);
 
-        talon.setNeutralMode(NeutralMode.Coast);
+    talon.config_kP(pidLoopIndex, p);
+    talon.config_kI(pidLoopIndex, i);
+    talon.config_kD(pidLoopIndex, d);
 
-        talon.config_IntegralZone(pidLoopIndex, 200, timeoutMs);
-    }
+    talon.setNeutralMode(NeutralMode.Coast);
 
-    void setSpeed(int posPer100Ms) {
-        talon.set(ControlMode.Velocity, posPer100Ms);
-    }
+    talon.config_IntegralZone(pidLoopIndex, 200, timeoutMs);
+  }
+
+  void setSpeed(int posPer100Ms) {
+    talon.set(ControlMode.Velocity, posPer100Ms);
+  }
 }
