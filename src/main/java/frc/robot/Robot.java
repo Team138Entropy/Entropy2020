5d01989060d74d223f8c7d9da269e3817501fcb1
Joe Caraccio <jacaraccio@gmail.com>
2020-01-28 19:30:18 -0500

    Working for test

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,6 +7,7 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -17,8 +18,8 @@ import edu.wpi.first.wpilibj.XboxController;
  * package after creating this project, you must also update the build.gradle file in the project.
  */
 public class Robot extends TimedRobot {
-  WPI_TalonSRX mTalonMaster = new WPI_TalonSRX(1);
-  WPI_TalonSRX mTalonSlave = new WPI_TalonSRX(3);
+  WPI_TalonSRX mTalonMaster = new WPI_TalonSRX(3);
+  WPI_TalonSRX mTalonSlave = new WPI_TalonSRX(1);
 
   XboxController mController = new XboxController(0);
 
@@ -31,44 +32,28 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     mTalonSlave.follow(mTalonMaster);
     mTalonMaster.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.QuadEncoder, 0, 0);
+
+
+
+    //fgain = (100% x 1023)/ nativeUnits
+    //fgain = (100% x 1023)/ 9323
+    //6.6
+    //mTalonMaster.config_kF(0, 6.6);
+    //mTalonMaster.set(ControlMode.PercentOutput, 1.0f);
+    //mTalonMaster.set(ControlMode.Position, )
   }
 
-  @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {}
 
   @Override
   public void teleopPeriodic() {
-    if (mController.getAButtonPressed()) {
-      value -= 0.05f;
-    }
-    if (mController.getBButtonPressed()) {
-      value += 0.05f;
-    }
 
-    value = Math.min(value, 1.0f);
-    value = Math.max(value, 0f);
 
-    System.out.println(
-        "Set output value to "
-            + value
-            + " at velocity "
-            + "1: "
-            + mTalonMaster.getSensorCollection().getAnalogInVel()
-            + " 2: "
-            + mTalonMaster.getSensorCollection().getPulseWidthPosition());
+    System.out.println("Encoder Velocity: " + mTalonMaster.getSensorCollection().getQuadratureVelocity());
 
-    // mTalonMaster.set(ControlMode.PercentOutput, value);
   }
 
   @Override
   public void testInit() {}
-
   @Override
   public void testPeriodic() {}
 
