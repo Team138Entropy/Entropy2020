34e307cd8d5fe5b44c921956f516fa271194580f
maxh65000 <booah8@gmail.com>
2020-01-31 19:48:34 -0500

    Little by little, more and more.

--- src/main/java/frc/robot/events/EventWatcherThread.java
+++ src/main/java/frc/robot/events/EventWatcherThread.java
@@ -1,11 +1,14 @@
 package frc.robot.events;
 
 import edu.wpi.first.wpilibj.command.Scheduler;
+import frc.robot.Logger;
+
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 /** Singleton thread for monitoring things. */
 public class EventWatcherThread extends Thread {
+  Logger mLogger = new Logger("eventwatcherthread");
 
   private ArrayList<Event> queue = new ArrayList<>();
   private LinkedHashMap<Event, Boolean> lastStateCache = new LinkedHashMap<>();
@@ -44,7 +47,7 @@ public class EventWatcherThread extends Thread {
   public void addEvent(Event e) {
     if (!queue.contains(e)) {
       queue.add(e);
-      System.out.println("Event added (" + queue.size() + " total)");
+      mLogger.info("Event added (" + queue.size() + " total)");
     }
   }
 }
