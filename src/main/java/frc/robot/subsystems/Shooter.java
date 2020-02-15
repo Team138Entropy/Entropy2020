4cea907f97cfb47ca093af7aa986c160f991688b
maxh65000 <booah8@gmail.com>
2020-02-14 21:31:09 -0500

    Fix type conversion error

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -100,7 +100,7 @@ public class Shooter extends Subsystem {
   /** Starts the roller. */
   public void start() {  
     double speed = SpeedLookupTable.getInstance().getSpeedFromDistance(mVision.calcTargetPosition().getDistance());
-    mRoller.setSpeed(Math.round(speed));
+    mRoller.setSpeed((int) Math.round(speed));
   }
 
   /** Stops the roller. */
