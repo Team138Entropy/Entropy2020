b6cd914ad17a88458acdc7de7e7957e4557ebe15
Will Leverone <barrelwolf38087@gmail.com>
2020-03-04 20:05:15 -0500

    Remove dysfunctional turning segment

--- src/main/java/frc/robot/auto/TurnSegment.java
+++ src/main/java/frc/robot/auto/TurnSegment.java
@@ -1,129 +1,88 @@
 package frc.robot.auto;
 
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import frc.robot.Config;
 import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.subsystems.Drive;
 import frc.robot.util.DriveSignal;
 
-/** A type of {@link Segment} for turning in place. */
 public class TurnSegment extends Segment {
-  private static final double ACCEPTABLE_ERROR = 2;
 
-  private double degrees; // For cloning
-
-  private int targetPosition;
-  private double minAcceptable, maxAcceptable;
-
-  private boolean done = false;
+  private Gyro gyro = Robot.getGyro();
   private Drive drive;
+
+  private double P, I, D;
+  private PIDController controller;
+
+  private double degrees;
 
   // This allows us to only log the encoder positions once every five ticks
   private int loggingCount = 0;
 
   // The number of ticks for which we've been within the acceptable range
   private int debounceCount = 0;
+  private boolean done = false;
 
   public TurnSegment(double degrees) {
-    // this.trackWidth = Config.getInstance().getDouble(Config.Key.ROBOT__REAL_TRACK_WIDTH);
-    // this.ticksPerMeter = Config.getInstance().getDouble(Config.Key.DRIVE__TICKS_PER_METER);
-
-    this.degrees = degrees % 360;
     this.drive = Drive.getInstance();
-    targetPosition = degreesToTicks(degrees);
+    
+    degrees = degrees % 360;
+    this.degrees = degrees;
 
-    // double acceptableError =
-    //    Config.getInstance().getDouble(Config.Key.AUTO__TURN_PID_ACCEPTABLE_ERROR);
-    this.minAcceptable = this.degrees - ACCEPTABLE_ERROR;
-    this.maxAcceptable = this.degrees + ACCEPTABLE_ERROR;
+    P = Config.getInstance().getDouble(Config.Key.AUTO__TURN_PID_P);
+    I = Config.getInstance().getDouble(Config.Key.AUTO__TURN_PID_I);
+    D = Config.getInstance().getDouble(Config.Key.AUTO__TURN_PID_D);
+
+    double tolerance = Config.getInstance().getDouble(Config.Key.AUTO__TURN_PID_ACCEPTABLE_ERROR);
+
+    this.controller = new PIDController(P, I, D);
+    this.controller.setSetpoint(degrees);
+    this.controller.setTolerance(tolerance);
   }
 
   @Override
   public void init() {
     logger.info("Initializing turn segment");
-    logger.info("Target angle: " + degrees + "deg");
-    logger.info("Target distance: " + targetPosition);
-
-    drive.configP(10);
-    drive.configI(0);
-
-    drive.zeroEncoders();
-    drive.setMotionMagicTarget(-targetPosition, targetPosition); // Counterclockwise
+    logger.info("Target: " + degrees + "deg");
+    gyro.reset();
   }
 
   @Override
   public void tick() {
-    int left = drive.getLeftEncoderDistance();
-    int right = -drive.getRightEncoderDistance();
+    double angle = gyro.getAngle();
 
+    double out = controller.calculate(angle);
+    
     if (++loggingCount > 5) {
-      logger.info(
-          "Encoder distances: ("
-              + drive.getLeftEncoderDistance()
-              + ", "
-              + -drive.getRightEncoderDistance()
-              + ")");
+      logger.info("Angle: " + angle);
       loggingCount = 0;
     }
+    
+    drive.arcadeHack(0, out, true);
 
-    double angle = calcAngle(-left, right);
-
-    if (angle > minAcceptable && angle < maxAcceptable) {
+    if (controller.atSetpoint()) {
       logger.verbose("Angle in acceptable range for " + ++debounceCount + " tick(s)");
+
+      logger.info("Angle: " + angle);
 
       if (debounceCount >= Constants.AUTO_DEBOUNCE_TICKS) {
         done = true;
         drive.setOpenLoop(DriveSignal.BRAKE);
       }
+    } else {
+      debounceCount = 0;
     }
   }
 
   @Override
   public boolean finished() {
-    if (done) {
-      logger.info("Turn segment finished");
-      drive.resetPID();
-    }
     return done;
   }
 
   @Override
   public Segment copy() {
     return new TurnSegment(degrees);
-  }
-
-  private int degreesToTicks(double theta) {
-    double r = drive.feetToTicks(Constants.REAL_TRACK_WIDTH) / 2.0;
-    return (int) Math.round((Math.PI * r * theta) / 180.0); // Magic!
-  }
-
-  private static class Point {
-    public double x, y;
-
-    public Point(double x, double y) {
-      this.x = x;
-      this.y = y;
-    }
-  }
-
-  // Heavy: Are you sure this will work?
-  // Medic: Ha ha, I have no idea!
-  private double calcAngle(double leftArc, double rightArc) {
-    double r = drive.feetToTicks(Constants.REAL_TRACK_WIDTH) / 2.0;
-    
-    double leftTheta = (degreesToTicks(180) + leftArc) / r;
-    double rightTheta = rightArc / r;
-
-    Point leftPoint = new Point(
-      Math.cos(leftTheta),
-      Math.sin(leftTheta)
-    );
-
-    Point rightPoint = new Point(
-      Math.cos(rightTheta),
-      Math.sin(rightTheta)
-    );
-
-    double slope = (rightPoint.y - leftPoint.y) / (rightPoint.x - leftPoint.x);
-
-    return Math.toDegrees(Math.atan(slope));
   }
 }
